7498512252f03b7e98c62682ffde1ffb1d9631ce
Sebastian De la Paz III <caesar.tx@gmail.com>
2020-07-19 13:03:03 -0500

    Used golint on hamming.go

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,18 +1,18 @@
-//Package to calculate the Hamming Distance between two DNA strands.
+//Package hamming is used to calculate the Hamming Distance between two DNA strands.
 package hamming
 
 import (
 	"errors"
 )
 
-//Calculate hamming distance
+//Distance function used to calculate hamming distance
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return -1, errors.New("length mismatch:  DNA strands provided are not of same length")
 	}
 
 	var distance int
-	for idx, _ := range a {
+	for idx := range a {
 		if a[idx] != b[idx] {
 			distance++
 		}
