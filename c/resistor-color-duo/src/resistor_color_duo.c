3f50f79a9fd62dd98a29127de0c8c89a852366df
Sebastian De la Paz III <caesar.tx@gmail.com>
2020-07-19 18:36:00 -0500

    Finished resistor-color and resistor-color-duo - C track

--- c/resistor-color-duo/src/resistor_color_duo.c
+++ c/resistor-color-duo/src/resistor_color_duo.c
@@ -1,1 +1,10 @@
 #include "resistor_color_duo.h"
+
+int color_code(resistor_band_t* colors){
+    int result = 0;
+    for(int i = 0; i < 2; i++){
+        result = result * 10 + colors[i];
+    }
+
+    return result;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -2,7 +2,19 @@
 #define RESISTOR_COLOR_H
 
 typedef enum {
-
+    BLACK = 0,
+    BROWN,
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    VIOLET,
+    GREY,
+    WHITE
 } resistor_band_t;
+
+resistor_band_t* colors();
+int color_code(resistor_band_t color);
 
 #endif
