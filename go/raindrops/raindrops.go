80f62b9bbc226389feab0d886168306654684364
Sebastian De la Paz III <caesar.tx@gmail.com>
2020-07-22 21:04:57 -0500

    Implemented raindrops version using string concatenation (Go track)

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -7,6 +7,26 @@ import (
 
 //Convert integer to raindrop-like string
 func Convert(input int) string {
+	var res string
+	if input%3 == 0 {
+		res += "Pling"
+	}
+	if input%5 == 0 {
+		res += "Plang"
+	}
+	if input%7 == 0 {
+		res += "Plong"
+	}
+
+	if len(res) == 0 {
+		res += strconv.Itoa(input)
+	}
+
+	return res
+}
+
+//Convert integer to raindrop-like string
+func convert2(input int) string {
 	var res bytes.Buffer
 	if input%3 == 0 {
 		res.WriteString("Pling")
