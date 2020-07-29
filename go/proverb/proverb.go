50fcf2c3bc97c679760d6364c7b6a55fb3ad8eeb
Sebastian De la Paz III <caesar.tx@gmail.com>
2020-07-28 22:09:35 -0500

    A few optimization to proverb.go (Go track)

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -7,15 +7,16 @@ package proverb
 
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
+	if len(rhyme) == 0 {
+		return []string{}
+	}
+
 	ans := make([]string, len(rhyme))
 
-	if len(ans) >= 2 {
-		for idx := 0; idx < len(ans)-1; idx++ {
-			ans[idx] = "For want of a " + rhyme[idx] + " the " + rhyme[idx+1] + " was lost."
-		}
+	for idx := 0; idx < len(ans)-1; idx++ {
+		ans[idx] = "For want of a " + rhyme[idx] + " the " + rhyme[idx+1] + " was lost."
 	}
-	if len(rhyme) > 0 {
-		ans[len(ans)-1] = "And all for the want of a " + rhyme[0] + "."
-	}
+
+	ans[len(ans)-1] = "And all for the want of a " + rhyme[0] + "."
 	return ans
 }
