467f23ecb56bf0240d96faaa1319c4e11cbec61f
Sebastian De La Paz <zebastian@fb.com>
2020-08-25 15:20:02 -0500

    Finished space-age (rust track)

---
+++ rust/space-age/target/debug/deps/space_age-1c049edfe4d1940a.d
@@ -0,0 +1,5 @@
+/Users/zebastian/Exercism/rust/space-age/target/debug/deps/space_age-1c049edfe4d1940a.rmeta: src/lib.rs
+
+/Users/zebastian/Exercism/rust/space-age/target/debug/deps/space_age-1c049edfe4d1940a.d: src/lib.rs
+
+src/lib.rs:
